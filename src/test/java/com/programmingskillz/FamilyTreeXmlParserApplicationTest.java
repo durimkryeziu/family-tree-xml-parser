a7e2af4c70c09c54e974079b8f6e2f481f9e8fcf
Durim Kryeziu <durimkryeziu7@gmail.com>
2017-10-21 23:57:36 +0200

    Annotate integration tests with @ActiveProfiles("test")

--- src/test/java/com/programmingskillz/FamilyTreeXmlParserApplicationTest.java
+++ src/test/java/com/programmingskillz/FamilyTreeXmlParserApplicationTest.java
@@ -3,9 +3,11 @@ package com.programmingskillz;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
+@ActiveProfiles("test")
 @RunWith(SpringRunner.class)
 public class FamilyTreeXmlParserApplicationTest {
 
--- src/test/java/com/programmingskillz/familytreexmlparser/web/DocumentControllerIntegrationTest.java
+++ src/test/java/com/programmingskillz/familytreexmlparser/web/DocumentControllerIntegrationTest.java
@@ -13,11 +13,13 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 /**
  * @author Durim Kryeziu
  */
+@ActiveProfiles("test")
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class DocumentControllerIntegrationTest {
