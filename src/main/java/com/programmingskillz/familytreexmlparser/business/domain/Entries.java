d8aee5f32bd6af85438d2fc2e0e3d40eb1df6034
Durim Kryeziu <durimkryeziu7@gmail.com>
2017-12-16 23:57:56 +0100

    refactor: Add convenience methods on Entries

--- src/main/java/com/programmingskillz/familytreexmlparser/business/domain/Entries.java
+++ src/main/java/com/programmingskillz/familytreexmlparser/business/domain/Entries.java
@@ -1,12 +1,10 @@
 package com.programmingskillz.familytreexmlparser.business.domain;
 
 import java.util.List;
+import java.util.stream.Collectors;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
-/**
- * @author Durim Kryeziu
- */
 @XmlRootElement
 public class Entries {
 
@@ -19,5 +17,22 @@ public class Entries {
   @XmlElement(name = "entry")
   public void setEntries(List<Entry> entries) {
     this.entries = entries;
+  }
+
+  public Entry getRoot() {
+    return this.getEntries().stream()
+        .filter(this::isParent)
+        .findFirst()
+        .orElse(null);
+  }
+
+  public List<Entry> getChildren() {
+    return this.getEntries().stream()
+        .filter(entry -> !isParent(entry))
+        .collect(Collectors.toList());
+  }
+
+  private boolean isParent(Entry entry) {
+    return entry.getParentName() == null;
   }
 }
